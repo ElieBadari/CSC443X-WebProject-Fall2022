759d0229242f8a6d68cebc8ad5cd7085745e8978
ElieBadari <elie.badari@hotmail.com>
2022-11-21 02:53:49 +0200

    completed signup api + succcessful testing [Backend : signup.php]

--- Backend/signup.php
+++ Backend/signup.php
@@ -9,7 +9,6 @@ $response = [];
 $flag = true;
 
 //validate user input for all three sign up fields
-
 if (isset($_POST["username"]) && $_POST["username"] != ""){
     $check = $mysqli->prepare("SELECT user_id FROM users WHERE username = ?");
     $param = filter_data($_POST["username"]);
@@ -47,10 +46,45 @@ if (isset($_POST["email"]) && $_POST["email"] != ""){
 }
 
 if (isset($_POST["password"]) && $_POST["password"] != ""){
-    $pass = hash('sha256', filter_data($_POST["password"]));
+    $password = hash('sha256', filter_data($_POST["password"]));
     $results["password"] = true;
 }else {
     $results["password"] = false;
 }
+
+//check if parameters are valid
+foreach ($results as $key=>$value){
+    if(!$value){
+        $flag = false;
+    }
+    $response[$key] = $value;
+}
+
+//insert user
+if (!$flag){
+    $response["Success"] = false;
+}else {
+    $query = $mysqli->prepare("INSERT INTO users (username,email,password) VALUES (?,?,?)");
+    $query->bind_param("sss",$username,$email,$password);
+    if ($query->execute()){
+        $response["Success"] = true;
+    }else {
+        $response["Success"] = false;
+    }
+    $query->close();
+
+    $query = $mysqli->prepare("SELECT user_id FROM users WHERE username = ?");
+    $query->bind_param("s",$username);
+    if ($query->execute()){
+        $result = $query->get_result();
+        $user_id = $result->fetch_assoc();
+        $response["user_id"] = $user_id["user_id"];
+    }
+}
+
+
+echo json_encode($response);
+
+
 
 
