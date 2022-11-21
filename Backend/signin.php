651f013b3ec5be34e0868843c4609c5329ace262
ElieBadari <elie.badari@hotmail.com>
2022-11-21 03:11:12 +0200

    completed signin api + succcessful testing [Backend : signin.php]

--- Backend/signin.php
+++ Backend/signin.php
@@ -1,14 +1,13 @@
 <?php
 
-include('connection.php');
-include('functions.php');
+include("connection.php");
+include("functions.php");
 
-$username = $email = $password = "";
+$username = $password = "";
 $results = [];
 $response = [];
 $flag = true;
 
-//validate user input for all three sign up fields
 if (isset($_POST["username"]) && $_POST["username"] != ""){
     $check = $mysqli->prepare("SELECT user_id FROM users WHERE username = ?");
     $param = filter_data($_POST["username"]);
@@ -16,10 +15,11 @@ if (isset($_POST["username"]) && $_POST["username"] != ""){
     if ($check->execute()){
         $check->store_result();
         if ($check->num_rows() == 1){
-            $results["username"] = false;
-        }else {
             $results["username"] = true;
             $username = filter_data($_POST["username"]);
+        }else {
+            $results["username"] = false;
+            
         }
     }
     $check->close();
@@ -27,63 +27,45 @@ if (isset($_POST["username"]) && $_POST["username"] != ""){
     $results["username"] = false;
 }
 
-if (isset($_POST["email"]) && $_POST["email"] != ""){
-    $check = $mysqli->prepare("SELECT user_id FROM users WHERE email = ?");
-    $param = filter_data($_POST["email"]);
-    $check->bind_param("s",$param);
+if (isset($_POST["password"]) && $_POST["password"] != ""){
+    $password = hash('sha256', filter_data($_POST["password"]));
+    $check = $mysqli->prepare("SELECT user_id FROM users WHERE username = ? AND password = ?");
+    $check->bind_param("ss",$username,$password);
     if ($check->execute()){
         $check->store_result();
         if ($check->num_rows() == 1){
-            $results["email"] = false;
+            $results["password"] = true;
         }else {
-            $results["email"] = true;
-            $email = filter_data($_POST["email"]);
+            $results["password"] = false;
         }
     }
     $check->close();
 }else {
-    $results["email"] = false;
-}
-
-if (isset($_POST["password"]) && $_POST["password"] != ""){
-    $password = hash('sha256', filter_data($_POST["password"]));
-    $results["password"] = true;
-}else {
     $results["password"] = false;
 }
 
-//check if parameters are valid
-foreach ($results as $key=>$value){
-    if(!$value){
+foreach ($results as $key => $value){
+    if (!$value){
         $flag = false;
     }
     $response[$key] = $value;
 }
 
-//insert user
 if (!$flag){
     $response["Success"] = false;
 }else {
-    $query = $mysqli->prepare("INSERT INTO users (username,email,password) VALUES (?,?,?)");
-    $query->bind_param("sss",$username,$email,$password);
-    if ($query->execute()){
-        $response["Success"] = true;
-    }else {
-        $response["Success"] = false;
-    }
-    $query->close();
-
     $query = $mysqli->prepare("SELECT user_id FROM users WHERE username = ?");
     $query->bind_param("s",$username);
     if ($query->execute()){
         $result = $query->get_result();
         $user_id = $result->fetch_assoc();
+        $response["Success"] = true;
         $response["user_id"] = $user_id["user_id"];
+    }else {
+        $response["Success"] = false;
     }
-}
 
+}
 echo json_encode($response);
 
 ?>
-
-
